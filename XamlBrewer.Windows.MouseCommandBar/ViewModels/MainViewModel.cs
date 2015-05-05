bb45677dcf7b6d5da34049eac2e112e8b73c7911
XamlBrewer <diederik_krols@hotmail.com>
2015-05-05 21:13:45 +0200

    Content.

--- XamlBrewer.Windows.MouseCommandBar/ViewModels/MainViewModel.cs
+++ XamlBrewer.Windows.MouseCommandBar/ViewModels/MainViewModel.cs
@@ -12,10 +12,10 @@
             {
                 return new List<CheeseViewModel>()
                 {
-                    new CheeseViewModel() { Name="Cheddar", Description="Blablabla"},
-                    new CheeseViewModel() { Name="Emmentaler", Description="Blablabla"},
-                    new CheeseViewModel() { Name="Gouda", Description="Blablabla"},
-                    new CheeseViewModel() { Name="Monterey Jack", Description="Blablabla"}
+                    new CheeseViewModel() { Name="Brie", Description="Q: What happened after an explosion at a French cheese factory?\nA: All that was left was de Brie."},
+                    new CheeseViewModel() { Name="Stilton", Description="Q: What hotel do mice stay in ?\nA: The Stilton."},
+                    new CheeseViewModel() { Name="Cheddar", Description="Q: When should you go on a cheese diet?\nA: If you need to Cheddar a few pounds."},
+                    new CheeseViewModel() { Name="Curds", Description="Q: What group of cheese has been known to fly?\nA: Curds of prey!"}
                 };
             }
         }
